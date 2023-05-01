2783e10eec0db8fd0772a2604005c8b5787a442a
Nero <1600959@uab.cat>
2023-05-01 13:02:46 +0200

    Login and main page skeleton

---
+++ js/login.js
@@ -0,0 +1,14 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if ( username == "user" && password == "pass"){
+    window.location = "main.html"; // Redirecting to other page.
+    return false;
+  }
+  else{
+    const wrongp = document.getElementById("wrongp");
+    wrongp.innerHTML = "Wrong username or password. Try again."
+  }
+}
